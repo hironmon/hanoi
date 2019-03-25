df3f43af16d226d77c274eb0194211631256fe53
Julien Anton <julienanton@MacBook-Pro-de-Julien-2.local>
2019-03-25 17:24:56 +0100

    test

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -6,14 +6,32 @@ import org.junit.Test;
 
 public class DisqueTest {
 
+	Disque petit = new Disque(1);
+	Disque moyen = new Disque(2);
+	Disque grand = new Disque(3);
+
 	@Test
-	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+	public void negativeCompareTo(){
+		int res = -1;
+		assertEquals(res, petit.compareTo(moyen));
+		assertEquals(res, petit.compareTo(grand));
+		assertEquals(res, moyen.compareTo(grand));
+	}
+
+	@Test
+	public void positiveCompareTo(){
+		int res = 1;
+		assertEquals(res, grand.compareTo(petit));
+		assertEquals(res, grand.compareTo(moyen));
+		assertEquals(res, moyen.compareTo(petit));
+	}
+
+	@Test
+	public void equalCompareTo(){
+		int res = 0;
+		assertEquals(res, petit.compareTo(petit));
+		assertEquals(res, moyen.compareTo(moyen));
+		assertEquals(res, grand.compareTo(grand));
 	}
 
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -4,11 +4,53 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class TourTest {
 
-	@Test
-	public void test() {
 
+	Disque petit = new Disque(1);
+	Disque moyen = new Disque(2);
+	Disque grand = new Disque(3);
+
+	@Test
+	public void empilerVide() {
+		Tour tour = new Tour();
+		assertEquals(true, tour.empiler(petit));
+		tour.depiler();
+		assertEquals(true, tour.empiler(moyen));
+		tour.depiler();
+		assertEquals(true, tour.empiler(grand));
+	}
+
+	@Test
+	public void empilerPlace(){
+		Tour tour = new Tour();
+		tour.empiler(grand);
+		assertEquals(true, tour.empiler(petit));
+		tour = new Tour();
+		tour.empiler(grand);
+		assertEquals(true, tour.empiler(moyen));
+		assertEquals(true, tour.empiler(petit));
+		tour = new Tour();
+		tour.empiler(moyen);
+		assertEquals(true, tour.empiler(petit));
+	}
+
+	@Test
+	public void empilerFail(){
+		Tour tour = new Tour();
+		tour.empiler(petit);
+		assertEquals(false, tour.empiler(moyen));
+		tour.depiler();
+		tour.empiler(grand);
+		tour.empiler(moyen);
+		tour.empiler(petit);
+		assertEquals(false, tour.empiler(petit));
+		assertEquals(false, tour.empiler(moyen));
+		assertEquals(false, tour.empiler(grand));
 	}
 
 }
